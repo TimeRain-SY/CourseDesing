4f4cfcd38d46ab82809fa8321142e3ab30e986ac
TimeRain-SY <2582275515@qq.com>
2023-06-07 00:30:03 +0800

    null

--- FourOperations/FourOperations_Demo/Main.java
+++ FourOperations/FourOperations_Demo/Main.java
@@ -12,13 +12,13 @@ public class Main {
         int num = 0;
         int answer = 0;
         int i = 0;
+        System.out.println("游戏规则: 根据公式输入正确答案,非整数结果取整数部分作答,答对一题加1分,答错一题不加分,共10题,总时间30秒");
         while (boo) {
             System.out.println("输入1 开始测试\n输入2 结束程序");
             num = e.Exceptions();
             switch (num) {
                 //开始程序
                 case 1: {
-                    System.out.println("游戏规则: 根据公式输入正确答案,非整数结果取整数部分作答,答对一题加1分,答错一题不加分,共10题,总时间30秒");
                     time.start();
                     for (i = 0; i < 10; i++) {
                         //生成题目
@@ -31,6 +31,8 @@ public class Main {
                         function.count(answer);
                         //到时结束
                         }
+                    System.out.println("作答结束,你的分数为: "+function.getScore());
+                    System.exit(0);
                     break;
                 }
                 //结束程序
